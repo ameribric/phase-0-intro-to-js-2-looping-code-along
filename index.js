257b43b68a1e5bb68385fc0eade8c7032f1ef035
Amer Ibric <ameribric@gmail.com>
2022-03-02 21:17:05 -0500

    commit tree 4f2d1bc2965cdd9b72980063e2f0c68fe310d1a6

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i=0; i<gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow`);
+//         debugger;
+// //     }
+// //     return gifts;
+// // }
+
+// wrapGifts(gifts);
+
+
+
+// // const names = ["Guadalupe", "Ollie", "Aki"];
+// const celebration = ["surprise"];
+
+
+function writeCards(names, celebration) {
+    let thankYouCards= []
+for (let n=0; n<names.length; n++) {
+  thankYouCards.push(`Thank you, ${names[n]}, for the wonderful ${celebration} gift!`)
+}
+return thankYouCards
+}
+
+
+
+
+
+
+
+// writeCards(names, celebration);
+
+// function writeCards(stringNames, eventName) {
+// let a = [0]
+// for (a = 3){
+
+
+function countDown() {
+    let x = 10
+    while (x>=0) {
+        console.log (x); x--;
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
